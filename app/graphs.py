558fd628e03eeecbba343dfb991b9ec5d59b6d29
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-10-31 13:06:57 -0400

    Broke chart generation out into a helper method

--- app/graphs.py
+++ app/graphs.py
@@ -8,56 +8,78 @@ rng = pd.date_range('1/1/2011', periods=25, freq='H')
 ts = pd.Series(np.random.randn(len(rng)), index=rng)
 
 
-def compile_graph_data(df):
-    graphs = [
-        dict(
-            data=[
-                dict(
-                    x=[1, 2, 3],
-                    y=[10, 20, 30],
-                    type='scatter'
-                ),
-            ],
-            layout=dict(
-                title='first graph'
-            )
-        ),
-
-        dict(
-            data=[
-                dict(
-                    x=[1, 3, 5],
-                    y=[10, 50, 30],
-                    type='bar'
-                ),
-            ],
-            layout=dict(
-                title='second graph'
-            )
-        ),
-
-        dict(
-            data=[
-                dict(
-                    x=df['gender'].sort_values().dropna().unique(),
-                    y=df['gender'].sort_values().value_counts(),
-                    type='bar'
-                ),
-            ],
-            layout=dict(
-                title='third graph'
-            )
-        ),
-
-        dict(
-            data=[
-                dict(
-                    x=ts.index,  # Can use the pandas data structures directly
-                    y=ts
+def get_graph_dict(x=None, y=None, graph_type=None, title='Insert title here'):
+    graph = dict(
+                data=[
+                    dict(
+                        x=x,
+                        y=y,
+                        type=graph_type
+                    ),
+                ],
+                layout=dict(
+                    title=title
                 )
-            ]
-        )
-    ]
+            )
+    return graph
+
+
+def compile_graph_data(df):
+    #     dict(
+    #         data=[
+    #             dict(
+    #                 x=[1, 2, 3],
+    #                 y=[10, 20, 30],
+    #                 type='scatter'
+    #             ),
+    #         ],
+    #         layout=dict(
+    #             title='first graph'
+    #         )
+    #     ),
+    #
+    #     dict(
+    #         data=[
+    #             dict(
+    #                 x=[1, 3, 5],
+    #                 y=[10, 50, 30],
+    #                 type='bar'
+    #             ),
+    #         ],
+    #         layout=dict(
+    #             title='second graph'
+    #         )
+    #     ),
+    #
+    #     dict(
+    #         data=[
+    #             dict(
+    #                 x=df['gender'].sort_values().dropna().unique(),
+    #                 y=df['gender'].sort_values().value_counts(),
+    #                 type='bar'
+    #             ),
+    #         ],
+    #         layout=dict(
+    #             title='third graph'
+    #         )
+    #     ),
+    #
+    #     dict(
+    #         data=[
+    #             dict(
+    #                 x=ts.index,  # Can use the pandas data structures directly
+    #                 y=ts
+    #             )
+    #         ]
+    #     )
+    # ]
+    graphs = (get_graph_dict([1, 2, 3], [10, 20, 30], 'scatter', 'first graph'),
+              get_graph_dict([1, 3, 5], [10, 20, 30], 'bar', 'second graph'),
+              get_graph_dict(df['gender'].sort_values().dropna().unique(),
+                             df['gender'].sort_values().value_counts(),
+                             'bar', 'third graph'),
+              get_graph_dict(ts.index, ts, title='fourth graph'))
+
 
     # Add "ids" to each of the graphs
     ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
@@ -68,6 +90,7 @@ def compile_graph_data(df):
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
 
     return ids, graphJSON
+
 
 
 
