d8aa6ab39fed1d7f2d918c71b2fb4347e3f8b182
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-11-18 15:38:42 -0500

    Fixed KeyError on tab click on home page

--- app/main/graphing/graphs.py
+++ app/main/graphing/graphs.py
@@ -2,17 +2,17 @@ from app.main.graphing.graph_tools import COLORS, generate_pie_chart_percentage_
     generate_horizontal_line_chart_dict, generate_non_pie_chart_dict
 
 
-def salary_for_years_of_exp(pd_series):
-    title = 'Compensation for Years of Professional Experience'
-    sal_exp = (pd_series.replace('[\$,)]', '', regex=True).replace('[(]', '-', regex=True).astype(float))
-    x = sal_exp['annual_amount_earned_from_all_tech_activities_combined']
-    y = sal_exp['years_of_professional_experience']
-    mode = 'markers'
-    xaxis_title = 'Total Compensation'
-    yaxis_title = 'Years of Professional Experience'
-    return generate_non_pie_chart_dict(title=title, x=x, y=y, mode=mode, xaxis_title=xaxis_title,
-                                                   yaxis_title=yaxis_title, color=COLORS['indians']['red'],
-                                                   line_color=COLORS['indians']['navy'])
+# def salary_for_years_of_exp(pd_series):
+#     title = 'Compensation for Years of Professional Experience'
+#     sal_exp = (pd_series.replace('[\$,)]', '', regex=True).replace('[(]', '-', regex=True).astype(float))
+#     x = sal_exp['annual_amount_earned_from_all_tech_activities_combined']
+#     y = sal_exp['years_of_professional_experience']
+#     mode = 'markers'
+#     xaxis_title = 'Total Compensation'
+#     yaxis_title = 'Years of Professional Experience'
+#     return generate_non_pie_chart_dict(title=title, x=x, y=y, mode=mode, xaxis_title=xaxis_title,
+#                                                    yaxis_title=yaxis_title, color=COLORS['indians']['red'],
+#                                                    line_color=COLORS['indians']['navy'])
 
 
 def get_title_and_df_key_from_tab_value(tab_values):
@@ -28,6 +28,7 @@ def get_graph_dict(title, df, df_keys, suffix='', yaxis_title=None):
         yaxis_title = df_keys[1]
         pd_series_1 = df[xaxis_title]
         pd_series_2 = df[yaxis_title]
+        pd_series_2 = (pd_series_2.replace('[\$,)]', '', regex=True).replace('[(]', '-', regex=True).astype(float))
         color = COLORS['indians']['red']
         line_color = COLORS['indians']['navy']
         return generate_non_pie_chart_dict(title=title, x=pd_series_1, y=pd_series_2, mode='markers',
