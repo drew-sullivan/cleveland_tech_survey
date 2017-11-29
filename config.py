4f4dee0b288cbb326fa0fb24c83b1131264dc470
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-11-29 08:31:33 -0500

    Getting ready for deploy

--- config.py
+++ config.py
@@ -43,6 +43,12 @@ class HerokuConfig(ProductionConfig):
 
     @classmethod
     def init_app(cls, app):
+        ProductionConfig.init_app(app)
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
         from werkzeug.contrib.fixers import ProxyFix
         app.wsgi_app = ProxyFix(app.wsgi_app)
 
