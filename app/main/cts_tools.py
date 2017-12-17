df4fe1dfa461b277f82d5ad944a281c24b98230b
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-12-16 20:32:09 -0500

    Added docstring to function. Renamed some vars

--- app/main/cts_tools.py
+++ app/main/cts_tools.py
@@ -18,7 +18,20 @@ def clean_df_for_printing(df):
     return cleaned_df
 
 
+def get_lowercase_version_of_title(string_to_change):
+    return string_to_change.replace(' ', '_').lower()
+
+
 def get_community_profile(df):
+    """
+    Keeps hierarchy ranking of categories and swaps out potential answers with either the mean or average,
+    depending on the question
+    Ex.
+        community_profile['Work']['Total Compensation'] returns the average of all survey respondents
+        community_profile['Technology']['Primary Programming Languages Used At Work'] return the most common language
+    :param pandas Dataframe:
+    :return dict:
+    """
     community_profile = cts
     categories = cts.keys()
     question_title = ''
@@ -26,19 +39,19 @@ def get_community_profile(df):
         questions = cts[category]
         for question in questions:
             question_title = question
-            question = question.replace(' ', '_').lower()
-            answer = df[question].value_counts().index[0]
+            lowercase_version_of_title = get_lowercase_version_of_title(question)
+            answer = df[lowercase_version_of_title].value_counts().index[0]
             if isinstance(answer, np.integer) or isinstance(answer, np.float):
-                average = df[question].mean()
+                average = df[lowercase_version_of_title].mean()
                 answer = round(average, 2)
             if isinstance(answer, basestring) and '|' in answer:
-                user_responses = transform_strings_to_lists(df[question])
+                user_responses = transform_strings_to_lists(df[lowercase_version_of_title])
                 flat_list = itertools.chain.from_iterable(user_responses)
                 c = Counter(flat_list)
                 list_of_least_common_elements = c.most_common()
                 answer = list_of_least_common_elements[0][0]
             if isinstance(answer, basestring) and '$' in answer:
-                transformed_series = transform_currency_to_ints(df[question])
+                transformed_series = transform_currency_to_ints(df[lowercase_version_of_title])
                 average = transformed_series.mean()
                 answer = '${:,.2f}'.format(average)
             community_profile[category][question_title] = answer
