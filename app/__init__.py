a011bbafdc05a552cc870f39bc0da22c9dace31e
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-01-01 21:16:42 -0500

    Turned off SSLify while not paying for Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -27,9 +27,10 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
-    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
-        from flask_sslify import SSLify
-        sslify = SSLify(app)
+    # Enable if pay Dinos turned on via Heroku
+    # if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+    #     from flask_sslify import SSLify
+    #     sslify = SSLify(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
